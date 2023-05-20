c8b5a03a27bb993ae0b02859798b5269f6ce2ac9
Andrew <mullinsa428@gmail.com>
2023-05-20 14:55:32 -0400

    The gameGrid div now populates with a grid of holes based on the grid input from the first page

--- gamePage.js
+++ gamePage.js
@@ -25,11 +25,30 @@ let gameGridArr;
 function init() {
     bonks = 0;
     //CITATION: array constructor https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array
+    // creates an array with h and w dimensions based on the grid input from the menu
     let gameGridline = Array(gridSize);
     gameGridline.fill(0);
     gameGridArr = Array(gridSize);
     gameGridArr.fill(gameGridline);
     console.log(gameGridArr);   
+
+    // populates the gameGrid div with divs for each hole with ids detailing where they are on the board
+    for (i=0; i<gridSize; i++) {
+        for (j=0; j<gridSize; j++) {
+            gameGrid.innerHTML += `<div class='col${j} gameHole' id='row${i}col$'>ROW${i}COL${j}</div>`; 
+        }
+    }
+
+    // defines the height and width of grid area based on the number of rows/columns and taking into account the grid gap
+    let boardWidth = (10*gridSize)+(gridSize-1);
+    let boardHeight = (5*gridSize)+(gridSize-1);
+    gameGrid.style.width = `${boardWidth}rem`;
+    gameGrid.style.height = `${boardHeight}rem`;
+    
+    // defines the grid parameters // using a similar method from the css in the connect four game
+    
+    gameGrid.style.gridTemplateRows = `repeat(${gridSize}, 5rem)`;
+    gameGrid.style.gridTemplateColumns = `repeat(${gridSize}, 10rem)`;
 
 }
 
